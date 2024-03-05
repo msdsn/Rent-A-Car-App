990e316a3d61061e664dc6902f697d18d589bf3c
msdsn <devsanms@outlook.com>
2024-03-05 15:54:35 +0300

    Users app oluşturuldu ve register serializer oluşturuldu

---
+++ users/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        password = validated_data.pop('password')
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(password)
+        user.save()
+        return user
+    
---
+++ users/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path, include
+urlpatterns = [
+    path('auth/', include('dj_rest_auth.urls')),
+]
